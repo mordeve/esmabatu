0d90ad4f030b6490a541d540ca70985cafd50e79
Batuhan Karaoglu <batuhan.karaoglu@ardictech.com>
2023-08-30 01:34:16 +0300

    update main page

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:esmabatu/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key, required this.timee}) : super(key: key);
+
+  final String timee;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(200),
+              bottomRight: Radius.circular(200),
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0xFFBEBEBE),
+                offset: Offset(10, 10),
+                blurRadius: 30,
+                spreadRadius: 1,
+              ),
+              BoxShadow(
+                color: Color.fromARGB(255, 191, 189, 192),
+                offset: Offset(-10, -10),
+                blurRadius: 30,
+                spreadRadius: 1,
+              ),
+            ]),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(12.0),
+              alignment: Alignment.center,
+              child: Text(
+                appName,
+                style: GoogleFonts.pacifico(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(12.0),
+              alignment: Alignment.center,
+              child: Text(
+                timee,
+                style: GoogleFonts.pacifico(fontSize: 20),
+              ),
+            ),
+          ],
+        ));
+  }
+}
